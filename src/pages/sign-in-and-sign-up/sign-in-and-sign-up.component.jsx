03e7817910dff59d561c3014bc9af2820b1f954d
Akshay Nande <akshaytnande@gmail.com>
2019-10-25 20:56:57 -0700

    implemented firbase utils and included ability to store authenticated users in to the firestore database

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 
 import "./sign-in-and-sign-up.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sing-up/sign-up.component";
 
 const SignInAndSignUp = () => (
   <div className="sign-in-and-sign-up">
-    <SignIn /> 
+    <SignIn />
+    <SignUp />
   </div>
 );
 
