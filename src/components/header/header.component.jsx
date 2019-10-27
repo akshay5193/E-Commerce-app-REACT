66353c06ce5dc6ba3abf8869d0eca1f4853f7686
Akshay Nande <akshaytnande@gmail.com>
2019-10-27 13:29:18 -0700

    added redux and implemented user reducer and user action to header and app components.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,36 +1,40 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from '../../firebase/firebase.utils';
 
-import "./header.styles.scss";
-import { auth } from "./../../firebase/firebase.utils";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo" />
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className="option" to="/contact">
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
-
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          {" "}
+        <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="/signin">
-          SIGNIN
+        <Link className='option' to='/signin'>
+          SIGN IN
         </Link>
       )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
+import React from 'react';
 
-import "./sign-in.styles.scss";
-import FormInput from "./../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-button.component";
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from "./../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-class SignIn extends Component {
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      password: ""
+      email: '',
+      password: ''
     };
   }
 
@@ -23,7 +24,7 @@ class SignIn extends Component {
 
     try {
       await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: "", password: "" });
+      this.setState({ email: '', password: '' });
     } catch (error) {
       console.log(error);
     }
@@ -37,34 +38,31 @@ class SignIn extends Component {
 
   render() {
     return (
-      <div className="sign-in">
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            type="email"
-            name="email"
-            value={this.state.email}
+            name='email'
+            type='email'
             handleChange={this.handleChange}
-            label="Email"
+            value={this.state.email}
+            label='email'
             required
           />
-          {/* <label htmlFor="email">Email</label> */}
           <FormInput
-            type="password"
-            name="password"
+            name='password'
+            type='password'
             value={this.state.password}
             handleChange={this.handleChange}
-            label="Password"
+            label='password'
             required
           />
-          {/* <label htmlFor="password">Password</label> */}
-
-          <div className="buttons">
-            <CustomButton type="submit"> SIGN IN </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              SIGN IN WITH GOOGLE
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
